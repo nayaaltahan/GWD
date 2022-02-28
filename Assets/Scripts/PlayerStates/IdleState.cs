2ef7f9cd0d2753390e7912c84133d0112fd9aa43
Naya <naya.altahan00@gmail.com>
2022-02-28 20:55:40 +0100

    add initial implementation of state machine

---
+++ Assets/Scripts/PlayerStates/IdleState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : PlayerState
+{
+    public override void EnterState(PlayerStateController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update(PlayerStateController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnCollisionEnter(PlayerStateController player, Collision collision)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ExitState(PlayerStateController player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
