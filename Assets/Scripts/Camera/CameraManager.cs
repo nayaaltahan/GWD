a3719179b8472dc14ff6302b83753382f7e34ad2
Pedro <pedro.rainha.dev@gmail.com>
2022-03-01 12:42:32 +0100

    Basic Scene additions

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraManager : MonoBehaviour
 {
     public static CameraManager instance;
+    [SerializeField] private CinemachineTargetGroup targetGroup;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +17,9 @@ public class CameraManager : MonoBehaviour
             Debug.LogError("More than one Camera Manager in the scene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddPlayersTargetGroup()
     {
-        
+
     }
+    
 }
