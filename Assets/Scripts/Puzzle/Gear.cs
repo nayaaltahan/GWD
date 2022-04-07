184db4a06a95f26abad51b4bd9805116eb15285d
Pedro Rainha <pedro.rainha.dev@gmail.com>
2022-04-07 23:36:30 +0200

    Zone 4 done

--- Assets/Scripts/Puzzle/Gear.cs
+++ Assets/Scripts/Puzzle/Gear.cs
@@ -6,11 +6,19 @@ public class Gear : PuzzleObject
 {
     [SerializeField] private Vector3 axis;
     [SerializeField] private float rotateSpeed;
+    [SerializeField] private bool rotate;
 
     public override void Interact()
     {
-        transform.Rotate(axis, rotateSpeed * Time.deltaTime);
+        if(!rotate)
+            transform.Rotate(axis, rotateSpeed * Time.deltaTime);
     }
 
     public override void OnPlateRelease() { }
+
+    private void Update()
+    {
+        if(rotate)
+            transform.Rotate(axis, rotateSpeed * Time.deltaTime);
+    }
 }
