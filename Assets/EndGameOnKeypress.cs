76514aec6c5c90ec429c639827331f1d5e45c98e
tobiasdn <tobiasdn@gmail.com>
2022-05-18 18:43:53 +0200

    Updates end screen

--- Assets/EndGameOnKeypress.cs
+++ Assets/EndGameOnKeypress.cs
@@ -15,10 +15,7 @@ public class EndGameOnKeypress : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
+
     }
 
 
